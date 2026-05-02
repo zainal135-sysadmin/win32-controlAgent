220021d843d730ded0eb2766aca7fb209875881f
zainal135-sysadmin <zainal135ms@gmail.com>
2026-05-02 13:55:39 +0700

    Update control.py

--- control/control.py
+++ control/control.py
@@ -1,11 +1,14 @@
 from services import workflow_serv as service
 
-def stats_app_check():
-    for proc in service.raw_stats_app_check():
-        yield {
-            "pid": proc.info["pid"],
-            "name": proc.info["name"],
-            "status": proc.info["status"],
-            "exe": proc.info["exe"]
-        }
-        
+class ControlLogic:
+    def __init__(self):
+        self.uname = uname
+    def stats_app_check():
+        for proc in service.raw_stats_app_check():
+            yield {
+                "pid": proc.info["pid"],
+                "name": proc.info["name"],
+                "status": proc.info["status"],
+                "exe": proc.info["exe"]
+            }
+            
